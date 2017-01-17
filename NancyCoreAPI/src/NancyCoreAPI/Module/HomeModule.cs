442fcf2319b6159479100e7ac760f5fb8cd8fb15
jalpesh vadgama <Jalpesh@dotnetjalps.com>
2017-01-17 22:50:34 +0530

    Added home module for nancy

---
+++ NancyCoreAPI/src/NancyCoreAPI/Module/HomeModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace NancyCoreAPI.Module
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get("/", args => "Hello word from nancy module.");
+        }
+    }
+}
